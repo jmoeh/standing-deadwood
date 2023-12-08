189cab4f01dd69919d1e004e52feadcfc004a971
Jakobus Möhring <jmoehring@live.de>
2023-12-08 13:19:36 +0100

    fix: update rasterize script

--- scripts/rasterize_labels.py
+++ scripts/rasterize_labels.py
@@ -16,48 +16,57 @@ from tqdm import tqdm
 
 def process_file(filename, df_meta):
     if filename.endswith(".gpkg"):
-        filepath = os.path.join(args.labels_dir, filename)
-        # Get metadata for current gpkg file
-        filename_map = filename.replace("_ortho_polygons.gpkg", "")
-        file_meta = df_meta.loc[df_meta["filename_map"] == filename_map].to_dict(
-            "records"
-        )[0]
-        out_image = np.zeros((file_meta["height"], file_meta["width"]), dtype=np.uint8)
-        transform = from_bounds(
-            north=file_meta["top"],
-            south=file_meta["bottom"],
-            west=file_meta["left"],
-            east=file_meta["right"],
-            width=file_meta["width"],
-            height=file_meta["height"],
-        )
-        # Read in gpkg file and determine if standing deadwood is present
-        layers = fiona.listlayers(filepath)
-        if "standing_deadwood" in layers:
-            gdf_label = gpd.read_file(filepath, layer="standing_deadwood")
-            gdf_label = gdf_label.to_crs("EPSG:8857")
-            # Rasterize polygons
-            for _, row in gdf_label.iterrows():
-                geom = mapping(row["geometry"])
-                mask = geometry_mask(
-                    [geom], transform=transform, invert=True, out_shape=out_image.shape
-                )
-                out_image[mask] = 1
+        if (
+            filename.replace("_ortho_polygons.gpkg", "")
+            in df_meta["filename_map"].values
+        ):
+            filepath = os.path.join(args.labels_dir, filename)
+            # Get metadata for current gpkg file
+            filename_map = filename.replace("_ortho_polygons.gpkg", "")
+            file_meta = df_meta.loc[df_meta["filename_map"] == filename_map].to_dict(
+                "records"
+            )[0]
+            out_image = np.zeros(
+                (file_meta["height"], file_meta["width"]), dtype=np.uint8
+            )
+            transform = from_bounds(
+                north=file_meta["top"],
+                south=file_meta["bottom"],
+                west=file_meta["left"],
+                east=file_meta["right"],
+                width=file_meta["width"],
+                height=file_meta["height"],
+            )
+            # Read in gpkg file and determine if standing deadwood is present
+            layers = fiona.listlayers(filepath)
+            if "standing_deadwood" in layers:
+                gdf_label = gpd.read_file(filepath, layer="standing_deadwood")
+                gdf_label = gdf_label.to_crs("EPSG:8857")
+                # Rasterize polygons
+                for _, row in gdf_label.iterrows():
+                    geom = mapping(row["geometry"])
+                    mask = geometry_mask(
+                        [geom],
+                        transform=transform,
+                        invert=True,
+                        out_shape=out_image.shape,
+                    )
+                    out_image[mask] = 1
 
-        # Save image
-        with rasterio.open(
-            os.path.join(args.out_dir, filename_map + "_ortho_8857_mask.tif"),
-            "w",
-            driver="GTiff",
-            compress="DEFLATE",
-            height=out_image.shape[0],
-            width=out_image.shape[1],
-            count=1,
-            dtype="uint8",
-            crs="EPSG:8857",
-            transform=transform,
-        ) as dst:
-            dst.write(out_image, 1)
+            # Save image
+            with rasterio.open(
+                os.path.join(args.out_dir, filename_map + "_ortho_8857_mask.tif"),
+                "w",
+                driver="GTiff",
+                compress="DEFLATE",
+                height=out_image.shape[0],
+                width=out_image.shape[1],
+                count=1,
+                dtype="uint8",
+                crs="EPSG:8857",
+                transform=transform,
+            ) as dst:
+                dst.write(out_image, 1)
 
 
 parser = argparse.ArgumentParser(description="Rasterize labels")
@@ -73,7 +82,7 @@ parser.add_argument(
     required=False,
 )
 parser.add_argument(
-    "-gw",
+    "-gr",
     "--gr_path",
     help="Path to the the gdal_rasterize binary",
     default="gdal_rasterize",
@@ -98,13 +107,6 @@ args = parser.parse_args()
 # add new filename map column to find metadata for each image
 df_meta = pd.read_csv(args.metadata_file)
 df_meta["filename_map"] = df_meta["filename"].str.replace("_ortho_8857.tif", "")
-
-# assert there is metadata for all gpkg files in given directory
-for filename in os.listdir(args.labels_dir):
-    assert filename.endswith(".gpkg")
-    assert (
-        filename.replace("_ortho_polygons.gpkg", "") in df_meta["filename_map"].values
-    )
 
 with tqdm(total=len(os.listdir(args.labels_dir))) as pbar:
     with concurrent.futures.ThreadPoolExecutor(max_workers=args.jobs) as executor:
