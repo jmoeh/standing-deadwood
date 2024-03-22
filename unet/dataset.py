dcc247b8892d791734bdac5b9ce8b2c6800a7dc2
Jakobus Möhring <jmoehring@live.de>
2024-03-22 10:58:17 +0100

    feat: update training

--- unet/dataset.py
+++ unet/dataset.py
@@ -5,73 +5,82 @@ import torch
 from PIL import Image
 import pandas as pd
 import numpy as np
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, Subset
 from torchvision.transforms import transforms
-from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
 
 
 class DeadwoodDataset(Dataset):
     def __init__(
         self,
-        register_file,
-        n_folds=10,
-        random_seed=1,
+        register_df,
+        test_size=0.3,
+        random_seed=10,
     ):
         super(DeadwoodDataset, self).__init__()
-        self.register_file = register_file
-        self.register_df = pd.read_csv(register_file)
+        self.register_df = register_df
         self.random_seed = random_seed
+        random.seed(self.random_seed)
         np.random.seed(self.random_seed)
-        
-        bins = np.arange(0.00, 0.22, 0.02) # increase stop value to 0.22 to include the last bin
-        
-        # group the tiles by the base images and perform kfold split on the base images
-        self.file_groups = defaultdict(lambda: defaultdict(list))
-        for index, register_row in self.register_df.iterrows():
-            base_file_name = register_row["base_file_name"]
-            resolution = register_row["resolution"]
-            bin_index = np.digitize(resolution, bins)
-            self.file_groups[base_file_name][bins[bin_index - 1]].append(index)
 
-        self.kfold = KFold(n_splits=n_folds, shuffle=True, random_state=random_seed)
-        self.folds = list(self.kfold.split(list(self.file_groups.keys())))
+        train_register_df, test_register_df = train_test_split(
+            register_df,
+            test_size=test_size,
+            random_state=self.random_seed,
+            stratify=register_df[["biome", "resolution_bin"]],
+        )
 
-    """Returns the train and test subsets for the given fold index"""
+        self.train_register_df = train_register_df
+        self.test_register_df = test_register_df
 
-    def get_fold(self, fold_idx, epochs=1, n_res_samples=1):
-        train_indices, test_indices = self.folds[fold_idx]
-        
-        train_files_epochs = []
-        resolution_groups = defaultdict(list)
-        for group_idx in train_indices:
-            for resolution, resolution_indices in self.file_groups[list(self.file_groups.keys())[group_idx]].items():
-                resolution_groups[resolution].extend(resolution_indices)
-        for _ in range(epochs):
-            train_files = []
-            for resolution, resolution_indices in resolution_groups.items():
-                sampled_indices = np.random.choice(resolution_indices, size=n_res_samples, replace=True)
-                train_files.extend(sampled_indices)
-            train_files_epochs.append(train_files)
-            
-        test_files = [
-            idx
-            for group_idx in test_indices
-            for resolution in self.file_groups[list(self.file_groups.keys())[group_idx]].values()
-            for idx in resolution
-        ]
-        return train_files_epochs, test_files
+        # get indices of rows from original dataframe
+        self.train_indices = self.train_register_df.index.tolist()
+        self.test_indices = self.test_register_df.index.tolist()
+
+    def get_train_test(self):
+        return Subset(self, self.train_indices), Subset(self, self.test_indices)
 
     def __getitem__(self, index):
         image_path = self.register_df.iloc[index]["file_path"]
         image = Image.open(image_path).convert("RGB")
-        
         mask_path = image_path.replace(".tif", "_mask.tif")
         mask = Image.open(mask_path).convert("L")
-
-        image_tensor = transforms.ToTensor()(image).float().contiguous()
-        mask_tensor = transforms.PILToTensor()(mask).squeeze(1).long().contiguous()
-
-        return image_tensor, mask_tensor
+        mutual_transforms = transforms.Compose(
+            [
+                transforms.RandomHorizontalFlip(),
+                transforms.RandomVerticalFlip(),
+            ]
+        )
+        image_transforms = transforms.Compose(
+            [
+                transforms.RandomAutocontrast(p=0.2),
+            ]
+        )
+        image_tensor, mask_tensor = RandomTransform(mutual_transforms)(
+            image_transforms(image), mask
+        )
+        image_tensor = transforms.ToTensor()(image_tensor).float().contiguous()
+        mask_tensor = (
+            transforms.PILToTensor()(mask_tensor).squeeze(1).long().contiguous()
+        )
+        return image_tensor, mask_tensor, self.register_df.iloc[index].to_dict()
 
     def __len__(self):
         return len(self.register_df)
+
+
+class RandomTransform:
+    def __init__(self, transform):
+        self.transform = transform
+
+    def __call__(self, image, mask):
+        seed = torch.randint(0, 2**32, (1,))
+        random_state = torch.random.fork_rng([seed.item()])
+
+        transformed_image = self.transform(image)
+
+        torch.random.set_rng_state(random_state)
+        transformed_mask = self.transform(mask)
+
+        torch.random.set_rng_state(random_state)
+        return transformed_image, transformed_mask
--- unet/dice_score.py
+++ unet/dice_score.py
@@ -11,15 +11,35 @@ def dice_coeff(
     # Average of Dice coefficient for all batches, or for a single mask
     assert input.size() == target.size()
     assert input.dim() == 3 or not reduce_batch_first
-
     sum_dim = (-1, -2) if input.dim() == 2 or not reduce_batch_first else (-1, -2, -3)
-
     inter = 2 * (input * target).sum(dim=sum_dim)
     sets_sum = input.sum(dim=sum_dim) + target.sum(dim=sum_dim)
     sets_sum = torch.where(sets_sum == 0, inter, sets_sum)
-
     dice = (inter + epsilon) / (sets_sum + epsilon)
     return dice.mean()
+
+def confusion_values(input: Tensor, target: Tensor):
+    assert input.size() == target.size()
+    sum_dim = (-1, -2)
+    
+    true_positives = torch.sum(input * target, dim=sum_dim)
+    false_positives = torch.sum(input * (1 - target), dim=sum_dim)
+    false_negatives = torch.sum((1 - input) * target, dim=sum_dim)
+
+    # calculate precision, recall, and f1 score
+    precision = true_positives / (true_positives + false_positives)
+    recall = true_positives / (true_positives + false_negatives)
+    
+    precision = precision.cpu()
+    recall = recall.cpu()
+    
+    f1 = 2 * (precision * recall) / (precision + recall)
+    
+    return {
+        "precision": precision,
+        "recall": recall,
+        "f1": f1,
+    }
 
 
 def dice_loss(input: Tensor, target: Tensor):
