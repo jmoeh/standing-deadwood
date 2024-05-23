dc4223743ee8d0d73c0dbb4765e3af9873922113
Jakobus Möhring <jmoehring@live.de>
2024-05-23 18:56:56 +0200

    fix: update notebooks

--- unet/evaluate.py
+++ unet/evaluate.py
@@ -1,20 +1,22 @@
+import numpy as np
+import pandas as pd
 import torch
 import torch.nn.functional as F
+from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-from unet.dice_score import dice_coeff
+from unet.dice_score import dice_loss, confusion_values
 
 
 @torch.inference_mode()
-def evaluate(net, criterion, dataloader, device, amp):
+def evaluate(net, criterion, dataloader: DataLoader, device, amp):
     net.eval()
     num_val_batches = len(dataloader)
-    dice_score = 0
-    dice_loss = 0
-
+    val_loss = 0
+    val_rows = []
     # iterate over the validation set
     with torch.autocast(device.type if device.type != "mps" else "cpu", enabled=amp):
-        for image, mask_true, _ in tqdm(
+        for images, masks_true, images_meta in tqdm(
             dataloader,
             total=num_val_batches,
             desc="Validation round",
@@ -22,26 +24,45 @@ def evaluate(net, criterion, dataloader, device, amp):
             leave=False,
         ):
             # move images and labels to correct device and type
-            image = image.to(
+            images = images.to(
                 device=device, dtype=torch.float32, memory_format=torch.channels_last
             )
-            mask_true = mask_true.to(device=device, dtype=torch.long).squeeze()
-
+            masks_true = masks_true.to(device=device, dtype=torch.long).squeeze()
             # predict the mask
-            mask_pred = net(image).squeeze()
+            masks_pred = net(images).squeeze()
             assert (
-                mask_true.min() >= 0 and mask_true.max() <= 1
+                masks_true.min() >= 0 and masks_true.max() <= 1
             ), "True mask indices should be in [0, 1]"
 
-            # apply sigmoid to the mask
-            mask_pred = (F.sigmoid(mask_pred) > 0.5).float()
-            # compute the Dice score
-            dice_score += dice_coeff(mask_pred, mask_true, reduce_batch_first=False)
-            # compute the Dice loss
-            item_loss = criterion(mask_pred, mask_true.float())
-            item_loss += 1 - dice_score
+            for tile_index in range(len(images)):
+                mask_pred = masks_pred[tile_index].squeeze(1)
+                mask_true = masks_true[tile_index].squeeze(1).float()
 
-            dice_loss += item_loss.item()
+                tile_ce_loss = criterion(mask_pred, mask_true)
+                tile_dice_loss = dice_loss(
+                    F.sigmoid(mask_pred), mask_true, reduce_batch_first=False
+                )
+                tile_loss = tile_ce_loss + tile_dice_loss
+
+                for threshold in np.arange(0.1, 0.9, 0.1):
+                    mask_pred_treshold = (masks_pred[tile_index] > threshold).float()
+                    precision, recall, f1 = confusion_values(
+                        mask_pred_treshold, mask_true
+                    )
+                    val_row = {
+                        "treshold": threshold,
+                        "precision": precision.item(),
+                        "recall": recall.item(),
+                        "f1": f1.item(),
+                        "ce_loss": tile_ce_loss.item(),
+                        "dice_loss": tile_dice_loss.item(),
+                        "biome": images_meta["biome"][tile_index].item(),
+                        "resolution_bin": images_meta["resolution_bin"][
+                            tile_index
+                        ].item(),
+                    }
+                    val_rows.append(val_row)
+                val_loss += tile_loss.item()
 
     net.train()
-    return dice_score / max(num_val_batches, 1), dice_loss / max(num_val_batches, 1)
+    return val_loss / (len(dataloader) * dataloader.batch_size), pd.DataFrame(val_rows)
