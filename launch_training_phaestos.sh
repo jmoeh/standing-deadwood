a401402433d76370cec86cbf11c49bfbc6571572
Clemens Mosig <clemens.mosig@uni-leipzig.de>
2025-01-04 12:57:30 +0100

    adjust for new batch size and two gpus

--- launch_training_phaestos.sh
+++ launch_training_phaestos.sh
@@ -1,20 +1,16 @@
-#!/bin/bash
+##!/bin/bash
 
 export NCCL_P2P_DISABLE=1
-
-EXPERIMENT="tversky_a04b06g2"
-
-conda activate sd-env-12
 
 LAUNCHER="accelerate launch \
     --multi_gpu \
     --mixed_precision=fp16 \
-    --num_processes=3
+    --num_processes=2
     --num_machines=1
     --rdzv_conf rdzv_backend=static \
-    --gpu_ids 1,2,3 \
-    /net/home/jmoehring/standing-deadwood/train.py \
-    --config /net/home/jmoehring/experiments/testing_runs/$EXPERIMENT/config.json
+    --gpu_ids 0,1 \
+    /net/home/cmosig/projects/standing-deadwood/train.py \
+    --config /net/home/cmosig/projects/standing-deadwood/config_standard.json
 "
 
 $LAUNCHER
